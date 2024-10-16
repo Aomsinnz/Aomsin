7902bf92bd881f5ca54eb0f0ac6690a1530f56a3
Aomsinnz <aomsinnz01@gmail.com>
2024-10-17 00:27:35 +0700

    Update and rename Lap1.c to Lap2.c

--- Lap1.c
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    printf("Hello World \n");
-    printf("My Name is Aomsin." );
-    
-    return 0;
-}
---
+++ Lap2.c
@@ -0,0 +1,9 @@
+char = input("Enter a character: ")
+int1, int2 = map(int, input("Enter two integers: ").split())
+float_num = float(input("Enter a floating-point number: "))
+string = input("Enter a string: "
+    
+print(f"You entered character: {char}")
+print(f"You entered integers: {int1} and {int2}")
+print(f"You entered floating-point number: {float_num:.2f}")
+print(f"You entered string: {string}")
